c23486c12ab50f18fc069d82741c0da288dbd11a
ClementsJR <clementsjr@bilbo.cs.hendrix.edu>
2015-03-16 21:46:41 -0500

    Packeting code is now cleaner, preserves filenames, and no longer supports the packeting of strings

--- src/packet/Packetizer.java
+++ src/packet/Packetizer.java
@@ -6,13 +6,10 @@ import java.io.IOException;
 import java.util.PriorityQueue;
 
 public class Packetizer {
-	private byte IDCount;
-	private PriorityQueue<Packet> /*filesIn,*/ packetsOut;
+	private PriorityQueue<Packet> packetsOut;
 	private int packetSize;
 	
 	public Packetizer(int packetSize) {
-		IDCount = 0;
-		//filesIn = new PriorityQueue<Packet>();
 		packetsOut = new PriorityQueue<Packet>();
 		this.packetSize = packetSize;
 	}
@@ -27,29 +24,25 @@ public class Packetizer {
 	
 	public void packetize(File f) {
 		FileReader reader;
+		int numParts = (int)(f.length()/(packetSize-16));
 		try {
 			reader = new FileReader(f);
-			char[] cbuf = new char[packetSize];
-			for(int i=0; i <= f.length()/packetSize; i++) {
-				reader.read(cbuf);
-				packetsOut.add(new Packet((byte)(f.hashCode() + IDCount),i, (int)(f.length()/packetSize), 'f', cbuf));
+			char[] data = new char[packetSize];
+			
+			String name = f.getName();
+			data = name.toCharArray();
+			packetsOut.add(new Packet(f.hashCode(), 0, numParts, name.length(), data));
+			
+			for(int i=1; i <= (int)(f.length()/(packetSize-16)); i++) {
+				int size = reader.read(data);
+				
+				if(size != -1)
+					packetsOut.add(new Packet(f.hashCode() , i, numParts, size, data));
+				else
+					packetsOut.add(new Packet(f.hashCode() , i, numParts, (int)(f.length()%(packetSize-16)), data));
 			}
-			IDCount++;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-	
-	public void packetize(String s) {
-		char[] cbuf = new char[packetSize];
-		for(int i=0; i<=s.length()/packetSize; i++) {
-			if((i+1)*packetSize <= s.length()) {
-				s.getChars(i*packetSize, (i+1)*packetSize, cbuf, 0);
-			} else if(i*packetSize < s.length()) {
-				s.getChars(i*packetSize, s.length(), cbuf, 0);
-			}
-			packetsOut.add(new Packet((byte)(s.hashCode() + IDCount), i, (int)(s.length()/packetSize), 's', cbuf));
-		}
-		IDCount++;
 	}
 }
