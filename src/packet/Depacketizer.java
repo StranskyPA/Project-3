c8e3e5c96b1a6144dbfdf3553128fb0559fdd8fd
StranskyPA <stranskypa@hendrix.edu>
2015-03-16 21:52:14 -0500

    Revert "Revert "Packeting code is now cleaner, preserves filenames, and no longer supports the packeting of strings""
    This reverts commit f436d80225d56afef5e3870d2aa0876c5262c62a.

--- src/packet/Depacketizer.java
+++ src/packet/Depacketizer.java
@@ -6,110 +6,50 @@ import java.io.IOException;
 import java.util.Hashtable;
 import java.util.PriorityQueue;
 
+import application.Controller;
+
 public class Depacketizer {
-	private PriorityQueue<Object> objectsOut;
-	private Hashtable<Byte, File> partialFiles;
-	private Hashtable<Byte, String> partialStrings;
+	//private PriorityQueue<Object> objectsOut;
+	private Controller c;
+	private Hashtable<Integer, File> partialFiles;
 	
-	public Depacketizer() {
-		objectsOut = new PriorityQueue<Object>();
-		partialFiles = new Hashtable<Byte, File>();
-		partialStrings = new Hashtable<Byte, String>();
+	public Depacketizer(Controller c) {
+		//objectsOut = new PriorityQueue<Object>();
+		this.c = c;
+		partialFiles = new Hashtable<Integer, File>();
 	}
 	
-	public boolean hasNextFile() {
+	/*public boolean hasNextFile() {
 		return !objectsOut.isEmpty();
 	}
 	
 	public Object getNextFile() {
 		return objectsOut.remove();
-	}
-	
-	private byte getID(Packet p) {
-		return p.get(0);
-	}
-	
-	private int getPart(Packet p) {
-		int part = p.get(1);
-		part = (part << 8) + p.get(2);
-		part = (part << 8) + p.get(3);
-		part = (part << 8) + p.get(4);
-		
-		return part;
-	}
-	
-	private int getNumParts(Packet p) {
-		int totalNumParts = p.get(5);
-		totalNumParts = (totalNumParts << 8) + p.get(6);
-		totalNumParts = (totalNumParts << 8) + p.get(7);
-		totalNumParts = (totalNumParts << 8) + p.get(8);
-		
-		return totalNumParts;
-	}
-	
-	private char getType(Packet p) {
-		return (char)(p.get(9));
-	}
-	
-	private char[] get_cbuf(Packet p) {
-		char[] cbuf = new char[p.length() -10];
-		for(int i=0; i<cbuf.length; i++) {
-			cbuf[i] = (char)(p.get(i+10));
-		}
-		return cbuf;
-	}
+	}*/
 	
 	public void depacketize(Packet p) {
-		if(getType(p) == 'f')
-			makeFile(p);
-		else
-			makeString(p);
-	}
-	
-	
-	private void makeString(Packet p) {
-		char[] cbuf = get_cbuf(p);
-		String s;
-		
-		if(getPart(p) > 0) {
-			s = partialStrings.get(getID(p));
-		} else {
-			s = "";
-			partialStrings.put(getID(p), s);
-		}
-		
-		for(int i=0; i<cbuf.length; i++) {
-			s += cbuf[i];
-		}
-		
-		if(getPart(p) == getNumParts(p)) {
-			objectsOut.add(s);
-			partialStrings.remove(getID(p));
-		}
-	}
-	
-	private void makeFile(Packet p) {
-		char[] cbuf = get_cbuf(p);
+		char[] data = p.getAllData();
 		File f;
 		
-		if(getPart(p) > 0) {
-			f = partialFiles.get(getID(p));
+		if(p.getPart() > 0) {
+			f = partialFiles.get(p.getID());
 		} else {
-			f = new File("" + getID(p));
-			partialFiles.put(getID(p), f);
+			f = new File(new String(data));
+			partialFiles.put(p.getID(), f);
 		}
 		
 		try {
 			FileWriter writer = new FileWriter(f);
-			writer.write(cbuf);
+			writer.write(data);
 			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		if(getPart(p) == getNumParts(p)) {
-			objectsOut.add(f);
-			partialFiles.remove(getID(p));
+		if(p.getPart() == p.getNumParts()) {
+			//objectsOut.add(f);
+			c.recieveMessage(f);
+			partialFiles.remove(p.getID());
 		}
 	}
 }
--- src/packet/Packet.java
+++ src/packet/Packet.java
@@ -1,34 +1,95 @@
 package packet;
 
-public class Packet {
-	private byte[] bytes;
+import java.net.DatagramPacket;
 
-	public Packet(byte id, int part, int totalNumParts, char type, char[] cbuf) {
-		bytes = new byte[cbuf.length + 10];
-		bytes[0] = id;
+public class Packet{
+	private byte[] bytes;
+	
+	private static final int ID_IDX = 0;
+	private static final int PART_IDX = 4;
+	private static final int NUM_PARTS_IDX = 8;
+	private static final int DATA_SIZE_IDX = 12;
+	private static final int DATA_START_IDX = 16;
+
+	public Packet(int id, int part, int totalNumParts, int dataSize, char[] data) {
+		bytes = new byte[data.length + 10];
 		
-		bytes[1] = (byte)(part >> 24);
-		bytes[2] = (byte)(part >> 16);
-		bytes[3] = (byte)(part >> 8);
-		bytes[4] = (byte)(part);
+		bytes[ID_IDX] = (byte)(id >> 24);
+		bytes[ID_IDX+1] = (byte)(id >> 16);
+		bytes[ID_IDX+2] = (byte)(id >> 8);
+		bytes[ID_IDX+3] = (byte)(id);
 		
-		bytes[5] = (byte)(totalNumParts >> 24);
-		bytes[6] = (byte)(totalNumParts >> 16);
-		bytes[7] = (byte)(totalNumParts >> 8);
-		bytes[8] = (byte)(totalNumParts);
+		bytes[PART_IDX] = (byte)(part >> 24);
+		bytes[PART_IDX+1] = (byte)(part >> 16);
+		bytes[PART_IDX+2] = (byte)(part >> 8);
+		bytes[PART_IDX+3] = (byte)(part);
 		
-		bytes[9] = (byte)(type);
+		bytes[NUM_PARTS_IDX] = (byte)(totalNumParts >> 24);
+		bytes[NUM_PARTS_IDX+1] = (byte)(totalNumParts >> 16);
+		bytes[NUM_PARTS_IDX+2] = (byte)(totalNumParts >> 8);
+		bytes[NUM_PARTS_IDX+3] = (byte)(totalNumParts);
 		
-		for(int i=10; i < bytes.length; i++) {
-			bytes[i] = (byte)(cbuf[i-10]);
+		bytes[DATA_SIZE_IDX] = (byte)(dataSize >> 24);
+		bytes[DATA_SIZE_IDX+1] = (byte)(dataSize >> 16);
+		bytes[DATA_SIZE_IDX+2] = (byte)(dataSize >> 8);
+		bytes[DATA_SIZE_IDX+3] = (byte)(dataSize);
+		
+		for(int i=DATA_START_IDX; i < bytes.length; i++) {
+			bytes[i] = (byte)(data[i - DATA_START_IDX]);
 		}
 	}
 	
-	public byte get(int index) {
-		return bytes[index];
+	public Packet(DatagramPacket p) {
+		bytes = p.getData();
 	}
 	
 	public int length() {
 		return bytes.length;
+	}
+	
+	public int getID() {
+		int id = bytes[ID_IDX];
+		id = (id << 8) + bytes[ID_IDX+1];
+		id = (id << 8) + bytes[ID_IDX+2];
+		id = (id << 8) + bytes[ID_IDX+3];
+		return id;
+	}
+	
+	public int getPart() {
+		int part = bytes[PART_IDX];
+		part = (part << 8) + bytes[PART_IDX+1];
+		part = (part << 8) + bytes[PART_IDX+2];
+		part = (part << 8) + bytes[PART_IDX+3];
+		return part;
+	}
+	
+	public int getNumParts() {
+		int numParts = bytes[NUM_PARTS_IDX];
+		numParts = (numParts << 8) + bytes[NUM_PARTS_IDX+1];
+		numParts = (numParts << 8) + bytes[NUM_PARTS_IDX+2];
+		numParts = (numParts << 8) + bytes[NUM_PARTS_IDX+3];
+		return numParts;
+	}
+	
+	public int getSize() {
+		int size = bytes[DATA_SIZE_IDX];
+		size = (size << 8) + bytes[DATA_SIZE_IDX+1];
+		size = (size << 8) + bytes[DATA_SIZE_IDX+2];
+		size = (size << 8) + bytes[DATA_SIZE_IDX+3];
+		return size;
+	}
+	
+	public char[] getAllData() {
+		char[] data = new char[getSize()];
+		
+		for(int i=0; i<getSize(); i++) {
+			data[i] = (char)(bytes[i+DATA_START_IDX]);
+		}
+		
+		return data;
+	}
+	
+	public DatagramPacket makeDatagramPacket() {
+		return new DatagramPacket(bytes, bytes.length);
 	}
 }
