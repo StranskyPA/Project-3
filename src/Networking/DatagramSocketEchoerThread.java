e1ce3150f6e8fc460bdc027d1aace735bf9485b8
StranskyPA <stranskypa@hendrix.edu>
2015-03-17 20:35:47 -0500

    More Changes for 3-17-15

--- src/Networking/DatagramSocketEchoerThread.java
+++ src/Networking/DatagramSocketEchoerThread.java
@@ -6,17 +6,20 @@ import java.io.*;
 import application.Controller;
 import packet.Depacketizer;
 import packet.Packet;
+import packet.Packetizer;
 
 public class DatagramSocketEchoerThread {
 	private DatagramSocket socket;
 	private DatagramPacket packet;
 	private Depacketizer depacketer;
+	private Packetizer packeter;
 	private Controller c;
 	private final static int PACKETSIZE = 512;
 	
 	public DatagramSocketEchoerThread(DatagramSocket newSocket) {
 		socket = newSocket;
 		packet = new DatagramPacket(new byte[PACKETSIZE], PACKETSIZE);;
+		packeter = new Packetizer(PACKETSIZE);
 		depacketer = new Depacketizer(c);
 	}
 	
@@ -25,6 +28,14 @@ public class DatagramSocketEchoerThread {
 		System.out.println("Accepted Packet From: " + socket.getInetAddress());
 		Packet newPacket = new Packet(packet);
 		depacketer.depacketize(newPacket);
+		
+		if (packeter.hasNextPacket()) {
+			try {
+				socket.send(packeter.getNextPacket().makeDatagramPacket());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public void getNewPacket() {
